c93c600bf0132154308bc776a50f1e6ea711d31f
tomasvicar <tomasvicar@gmail.com>
2020-04-06 23:16:07 +0200

    non pretrained  0.76311535/0.7272554585131986  model

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import driver_py
 import evaluate_12ECG_score
+import evaluate_12ECG_score_nan
 import numpy as np
 
 data_path='../Training_WFDB'
@@ -11,7 +12,7 @@ res_path='../res'
 driver_py.driver(data_path,res_path)
 
 
-auroc,auprc,accuracy,f_measure,f_beta,g_beta = evaluate_12ECG_score.evaluate_12ECG_score(data_path,res_path)
+auroc,auprc,accuracy,f_measure,f_beta,g_beta = evaluate_12ECG_score_nan.evaluate_12ECG_score(data_path,res_path)
 
 output_string = 'AUROC|AUPRC|Accuracy|F-measure|Fbeta-measure|Gbeta-measure\n{:.3f}|{:.3f}|{:.3f}|{:.3f}|{:.3f}|{:.3f}'.format(auroc,auprc,accuracy,f_measure,f_beta,g_beta)
 
